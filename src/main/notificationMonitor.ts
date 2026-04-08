924f7193cd6eb2ca7497a76a6183a629c062dd3d
terotero0120 <xyz.012033@gmail.com>
2026-04-08 15:08:38 +0900

    refactor: simplify notification monitor imports and remove eval usage

--- src/main/notificationMonitor.ts
+++ src/main/notificationMonitor.ts
@@ -1,15 +1,13 @@
 import type { BaseNotificationMonitor } from './monitors/base';
+import { MacNotificationMonitor } from './monitors/mac';
+import { WindowsNotificationMonitor } from './monitors/windows';
 
 export type { NotificationData } from './monitors/base';
 export type { BaseNotificationMonitor };
 
 export function createNotificationMonitor(): BaseNotificationMonitor {
   if (process.platform === 'win32') {
-    // eslint-disable-next-line no-eval
-    const { WindowsNotificationMonitor } = (eval('require') as NodeRequire)('./monitors/windows');
     return new WindowsNotificationMonitor();
   }
-  // eslint-disable-next-line no-eval
-  const { MacNotificationMonitor } = (eval('require') as NodeRequire)('./monitors/mac');
   return new MacNotificationMonitor();
 }
